e9e6ebf44da7e76df81776fea4b6876ae13b9d54
Frederick Price <rprice@pricemail.ca>
2023-02-28 19:12:51 -0500

    Get things compiling with strict settings

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,24 @@
-module Main where
+{-# LANGUAGE Unsafe #-}
+
+module Main (main) where
+
+import Database.HDBC
+import Database.HDBC.Sqlite3
+import Prelude
+
+-- add :: Num a => a -> a -> a
+-- add a b = a + b
 
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main =
+  do
+    putStrLn "About to open sqlite database test1.db"
+    conn <- connectSqlite3 "test1.db"
+    putStrLn "Creating a table"
+    _ <- run conn "CREATE TABLE IF NOT EXISTS test (id INTEGER NOT NULL, desc VARCHAR(80))" []
+    putStrLn "Inserting data"
+    _ <- run conn "INSERT INTO test (id) VALUES (?)" [toSql (1 :: Int)]
+    putStrLn "Commiting"
+    commit conn
+    putStrLn "Disconnecting"
+    disconnect conn
