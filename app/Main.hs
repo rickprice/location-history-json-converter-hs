4c53dc6844eb78c79e6c189002180c2019677f67
Frederick Price <rprice@pricemail.ca>
2023-11-16 12:52:54 -0500

    Do Formoulu formatting

--- app/Main.hs
+++ app/Main.hs
@@ -1,26 +1,27 @@
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE ImportQualifiedPost #-}
 {-# LANGUAGE Unsafe #-}
-{-# LANGUAGE DeriveGeneric #-}
 
 module Main (main) where
 
 import Codec.Archive.Tar qualified as Tar
+
 -- import Codec.Archive.Tar.Entry qualified as Tar
 
 -- import GHC.RTS.Flags (DoCostCentres (CostCentresJSON))
 
 import Codec.Archive.Tar.Index qualified as TAR
 import Codec.Compression.GZip qualified as GZip
-import Data.Aeson (fromJSON, ToJSON (toJSON), encode, eitherDecode)
+import Data.Aeson (ToJSON (toJSON), eitherDecode, encode, fromJSON)
 import Data.ByteString.Lazy qualified as BS
 import Model
 import Prelude
 
-import Data.Text (Text,pack)
+import Data.Text (Text, pack)
 
--- | This is like the standard 'foldr' function on lists, but for 'Entries'.
--- Compared to 'foldEntries' it skips failures.
+{- | This is like the standard 'foldr' function on lists, but for 'Entries'.
+ Compared to 'foldEntries' it skips failures.
+-}
 foldEntriesIgnoreFailure :: (Tar.Entry -> a -> a) -> a -> Tar.Entries e -> a
 foldEntriesIgnoreFailure next done = fold
   where
@@ -29,8 +30,8 @@ foldEntriesIgnoreFailure next done = fold
     fold (Tar.Fail _) = done
 
 -- Convert an entry to its filepath
-entryToPath :: Tar.Entry -> String
-entryToPath entry = show $ Tar.entryPath entry
+-- entryToPath :: Tar.Entry -> String
+-- entryToPath entry = show $ Tar.entryPath entry
 
 -- Convert an entry to its ByteString
 entryToByteString :: Tar.Entry -> BS.ByteString
@@ -43,50 +44,31 @@ entryToByteString entry = extractFileData (Tar.entryContent entry)
 -- Is this Entry the location data we are looking to export
 entryIsLocationData :: Tar.Entry -> Bool
 entryIsLocationData e = case Tar.entryContent e of
-  Tar.NormalFile _ _ -> doesPathMatch (Tar.entryPath e)
-  _ -> False
+    Tar.NormalFile _ _ -> doesPathMatch (Tar.entryPath e)
+    _ -> False
   where
     doesPathMatch :: String -> Bool
     doesPathMatch p = "Takeout/Location History/Records.json" == p
-
--- Code to process the incoming JSON
-decodeJSON bs = decodeJSON bs :: Model
-
--- data Locations = Locations
---   { locationsAccuracy :: Int,
---     locationsTimestamp :: Text,
---     locationsLongitudeE7 :: Int,
---     locationsLatitudeE7 :: Int
---   }
-
 
 -- location = Locations 1 (pack "test") 2 3
 -- model = Model [location]
 
 main :: IO ()
 main = do
-  fileContent <- GZip.decompress <$> BS.readFile "takeout.tgz"
-  let entries = Tar.read fileContent
-  let entryList = foldEntriesIgnoreFailure (:) [] entries
-  -- let filteredEntryList = map (id . entryToByteString) (filter entryIsLocationData entryList)
-  -- let filteredEntryList = map (decodeJSON . entryToByteString) (filter entryIsLocationData entryList)
-  -- let singleEntry = take 1 filteredEntryList
-  -- print (singleEntry)
-  -- print (model)
-  -- print (encode (toJSON model))
-  let locationRecordFiles = map (entryToByteString) (filter entryIsLocationData entryList)
-  let locationRecordFile = pure (head locationRecordFiles)
-  -- let decodedJSON = decodeJSON locationRecordFile
-  print ("starting")
-  -- Get JSON data and decode it
-  -- d <- (eitherDecode <$> locationRecordFile) :: IO (Either String [Model])
-  d <- (eitherDecode <$> locationRecordFile) :: IO (Either String Model)
-  -- If d is Left, the JSON was malformed.
-  -- In that case, we report the error.
-  -- Otherwise, we perform the operation of
-  -- our choice. In this case, just print it.
-  case d of
-    Left err -> putStrLn err
-    Right ps -> print ps
+    fileContent <- GZip.decompress <$> BS.readFile "takeout.tgz"
+    let entries = Tar.read fileContent
+    let entryList = foldEntriesIgnoreFailure (:) [] entries
+    let locationRecordFiles = map entryToByteString (filter entryIsLocationData entryList)
+    let locationRecordFile = pure (head locationRecordFiles)
+    print "starting"
+    -- Get JSON data and decode it
+    d <- (eitherDecode <$> locationRecordFile) :: IO (Either String Model)
+    -- If d is Left, the JSON was malformed.
+    -- In that case, we report the error.
+    -- Otherwise, we perform the operation of
+    -- our choice. In this case, just print it.
+    case d of
+        Left err -> putStrLn err
+        Right ps -> print ps
 
-  print ("finished")
+    print "finished"
--- app/Model.hs
+++ app/Model.hs
@@ -1,27 +1,27 @@
+{-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RecordWildCards #-}
-{-# LANGUAGE DeriveGeneric #-}
 
 module Model where
 
 import Data.Aeson (FromJSON (..), ToJSON (..), Value (..), object, (.:), (.=))
 import Data.Aeson.Types (prependFailure, typeMismatch)
 import Data.Text (Text)
-import Prelude
 import GHC.Generics
+import Prelude
 
 data Location = Location
-  { timestamp :: Maybe Text,
-    longitudeE7 :: Maybe Int,
-    latitudeE7 :: Maybe Int
-  }
-  deriving (Show, Eq, Ord, Generic)
+    { timestamp :: Maybe Text
+    , longitudeE7 :: Maybe Int
+    , latitudeE7 :: Maybe Int
+    }
+    deriving (Show, Eq, Ord, Generic)
 
 data Model = Model
-  { locations :: [Location]
-  }
-  deriving (Show, Eq, Ord, Generic)
+    { locations :: [Location]
+    }
+    deriving (Show, Eq, Ord, Generic)
 
 instance ToJSON Location
 instance FromJSON Location
