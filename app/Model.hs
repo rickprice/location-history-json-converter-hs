d638c3cb0e0c9ebca4e2539f7dcfcb6de3d5522a
Frederick Price <rprice@pricemail.ca>
2023-11-17 10:59:16 -0500

    Try loading the timestamp from the Google data

--- app/Model.hs
+++ app/Model.hs
@@ -3,23 +3,24 @@
 {-# LANGUAGE Unsafe #-}
 {-# LANGUAGE DerivingStrategies #-}
 
-module Model (Location, LocationMaybe, Model, locations) where
+module Model (LocationMaybe, Model, locations, isComplete) where
 
 import Data.Aeson
 import Data.Text (Text)
 import GHC.Generics
 import Prelude
+import Data.Time.Clock
 
-data Location = Location
-    { timestamp :: !Text
-    , longitudeE7 :: Int
-    , latitudeE7 :: Int
-    }
-    deriving stock (Show, Eq, Ord)
-    deriving stock (Generic)
+-- data Location = Location
+--     { timestamp :: !Text
+--     , longitudeE7 :: Int
+--     , latitudeE7 :: Int
+--     }
+--     deriving stock (Show, Eq, Ord)
+--     deriving stock (Generic)
 
 data LocationMaybe = LocationMaybe
-    { timestamp :: Maybe Text
+    { timestamp :: Maybe UTCTime
     , longitudeE7 :: Maybe Int
     , latitudeE7 :: Maybe Int
     }
@@ -36,3 +37,15 @@ instance ToJSON LocationMaybe
 instance FromJSON LocationMaybe
 instance ToJSON Model
 instance FromJSON Model
+
+
+isComplete :: LocationMaybe -> Bool
+isComplete x = case (t,lo,la) of
+                (Nothing,_,_) -> False
+                (_,Nothing,_) -> False
+                (_,_,Nothing) -> False
+                (_,_,_) -> True
+                where
+                    t = timestamp x
+                    lo =longitudeE7 x
+                    la =latitudeE7 x
